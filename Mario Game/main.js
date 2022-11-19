b09ac7abd942584bae4c651ec014f14f12260100
Asyart <98713531+Asyart@users.noreply.github.com>
2022-11-19 17:51:32 +0300

    mario_push

--- Mario Game/main.js
+++ Mario Game/main.js
@@ -1,12 +1,16 @@
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = 1024;
+canvas.height = 576;
 
 const gravity = 4.5
+
+//-----------------------------------class-------------------------------------------
 class Player {
     constructor() {
+        this.speed = 10
         this.position = {
             x: 100,
             y: 100
@@ -15,61 +19,137 @@ class Player {
             x: 0,
             y: 0
         }
-        this.width = 30
-        this.height = 30
+        this.width = 66
+        this.height = 150
+        this.image6 = image6
+        this.frames = 0
+        this.sprite = {
+            stand: {
+                right: image6,
+                left: image5,
+                cropWidth: 177,
+                width: 66
+            },
+            run: {
+                right: image8,
+                left: image7,
+                cropWidth: 341,
+                width:127.875
+            }
+        }
+
+        this.currentSprite = this.sprite.stand.right
+        this.currentSpriteCropWidth = 177
     }
 
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y,
-            this.width, this.height
-        )
+        c.drawImage(
+            this.currentSprite,
+            this.currentSpriteCropWidth * this.frames,
+            0,
+            this.currentSpriteCropWidth,
+            400,
+            this.position.x,
+            this.position.y, 
+            this.width, 
+            this.height
+            )
     }
 
     update() {
+        this.frames++
+        if(this.frames > 59 && (this.currentSprite === this.sprite.stand.right 
+            || this.currentSprite === this.sprite.stand.left)
+            )
+        this.frames = 0
+        else if(this.frames > 29 && (this.currentSprite === this.sprite.run.right 
+            || this.currentSprite === this.sprite.run.left)
+            )
+        this.frames = 0
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
             this.velocity.y += gravity
-        else
-            this.velocity.y = 0
     }
 }
-
+//--------------------------------------------class----------------------------------------
 class Platform {
     constructor({ x, y, image }) {
         this.position = {
             x,
             y
         }
-       
-        this.width = 200
-        this.height = 20
-        
+
         this.image = image
-        console.log(image.width)
+        image.addEventListener('load', () => {
+            this.width = image.width;
+            this.height = image.height;
+        });
+
     }
 
     draw() {
-        
         c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
-const image = new Image()
-image.src = 'platform.png';
+class GenericObject {
+    constructor({ x, y, image }) {
+        this.position = {
+            x,
+            y
+        }
 
-const player = new Player();
-const platforms = [new Platform({
-    x:200,
-    y:100,
-    image
-}),
-    new Platform({x: 500, y:200, image})]
+        this.image = image
+        image.addEventListener('load', () => {
+            this.width = image.width;
+            this.height = image.height;
+        });
 
+    }
 
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+//-----------------------------------------------------------
+//objects for images
+let image = new Image()
+image.src = 'images/platform.png';
+
+let image2 = new Image()
+image2.src = 'images/hills.png';
+
+let image3 = new Image()
+image3.src = 'images/background.png';
+
+let image4 = new Image()
+image4.src = 'images/platformSmallTall.png';
+
+let image5 = new Image()
+image5.src = 'images/spriteStandLeft.png';
+
+let image6 = new Image()
+image6.src = 'images/spriteStandRight.png';
+
+let image7 = new Image()
+image7.src = 'images/spriteRunLeft.png';
+
+let image8 = new Image()
+image8.src = 'images/spriteRunRight.png';
+
+//------------------------------------------------------------
+
+//objects
+let player = new Player();
+let platforms = []
+
+let genericObjects = [] // عندي مشكله هنا بالضبط لان الصور الان طريقة تخزينها عندي تختلف عن طريقة التوتريال
+
+let lastKey
 const keys = {
     right: {
         pressed: false
@@ -81,38 +161,97 @@ const keys = {
 
 let scrollOffset = 0
 
+//---------------------method for restart the game when lose constion acheived------------------
+function init() {
+
+//objects for images
+image = new Image()
+image.src = 'images/platform.png';
+
+image2 = new Image()
+image2.src = 'images/hills.png';
+
+image3 = new Image()
+image3.src = 'images/background.png';
+
+image4 = new Image()
+image4.src = 'images/platformSmallTall.png';
+
+//objects
+player = new Player();
+ platforms = [
+    new Platform({ x: 580 *4 +300 - 2 + 580 - 291, y: 270, image: image4 })
+    ,
+    new Platform({x: -1, y: 470, image})
+    ,
+    new Platform({ x: 580 - 2.5, y: 470, image })
+    ,
+    new Platform({ x: 580 *2 + 200, y: 470, image })
+    ,
+    new Platform({ x: 580 *3 + 350, y: 470, image })
+    ,
+    new Platform({ x: 580 *4 +300 - 2, y: 470, image })
+    ,
+    new Platform({ x: 580 *5 + 600 - 2, y: 470, image })
+]
+
+ genericObjects = [
+    new GenericObject({x:-1, y:-1, image: image3})
+,
+    new GenericObject({x:-1, y:-1, image: image2})
+] // عندي مشكله هنا بالضبط لان الصور الان طريقة تخزينها عندي تختلف عن طريقة التوتريال
+
+ scrollOffset = 0
+
+}
+
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    genericObjects.forEach((genericObject) => {
+        genericObject.draw()
+    })
+
     platforms.forEach(platform => {
         platform.draw()
     })
 
+    player.update()
+
     if (keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = 5
-    } else if (keys.left.pressed && player.position.x > 100) {
-        player.velocity.x = -5
+        player.velocity.x = player.speed
+    } else if ((keys.left.pressed && player.position.x > 100) || (keys.left.pressed && scrollOffset == 0
+    && player.position.x > 0) ) {
+        player.velocity.x = -player.speed
     } else {
         player.velocity.x = 0
 
         if (keys.right.pressed) {
-            scrollOffset += 5
+            scrollOffset += player.speed
 
             platforms.forEach(platform => {
-                platform.position.x -= 5
+                platform.position.x -= player.speed
+            })
+            genericObjects.forEach(genericObject => {
+                genericObject.position.x -= player.speed * 0.66
             })
 
-        } else if (keys.left.pressed) {
-            scrollOffset -= 5
+        } else if (keys.left.pressed && scrollOffset > 0) {
+            scrollOffset -= player.speed
 
             platforms.forEach(platform => {
-                platform.position.x += 5
+                platform.position.x += player.speed
+            })
+
+            genericObjects.forEach(genericObject => {
+                genericObject.position.x += player.speed * 0.66
             })
         }
     }
 
-    
+
     //platform collision detection
     platforms.forEach(platform => {
         if (player.position.y + player.height <= platform.position.y && player.position.y +
@@ -122,10 +261,42 @@ function animate() {
         }
     })
 
-    if(scrollOffset > 2000 )
-    console.log('you win')
-}   
+// Sprite switching !!
 
+    if(keys.right.pressed && lastKey === 'right' && player.currentSprite !== player.sprite.run.right)
+    {
+        player.frames = 1
+        player.currentSprite = player.sprite.run.right
+            player.currentSpriteCropWidth = player.sprite.run.cropWidth
+            player.width = player.sprite.run.width
+    } else if(keys.left.pressed && lastKey === 'left' && player.currentSprite !== player.sprite.run.left){
+        player.currentSprite = player.sprite.run.left
+            player.currentSpriteCropWidth = player.sprite.run.cropWidth
+            player.width = player.sprite.run.width
+    } else if(!keys.left.pressed && lastKey === 'left' && player.currentSprite !== player.sprite.stand.left){
+        player.currentSprite = player.sprite.stand.left
+            player.currentSpriteCropWidth = player.sprite.stand.cropWidth
+            player.width = player.sprite.stand.width
+    } else if(!keys.right.pressed && lastKey === 'right' && player.currentSprite !== player.sprite.stand.right){
+        player.currentSprite = player.sprite.stand.right
+            player.currentSpriteCropWidth = player.sprite.stand.cropWidth
+            player.width = player.sprite.stand.width
+    } 
+
+// win condition
+    if (scrollOffset > 580 * 5 + 300 - 2) {
+        window.open('https://tiny-faun-4a2ea5.netlify.app','_blank');
+        console.log('you win')
+    }
+
+//lose condtion 
+    if(player.position.y > canvas.height){
+        init()
+    }
+
+}
+//------------------calling methods for starting the game---------
+init()
 animate()
 
 addEventListener('keydown', ({ keyCode }) => {
@@ -134,6 +305,7 @@ addEventListener('keydown', ({ keyCode }) => {
         case 65:
             console.log('left')
             keys.left.pressed = true
+            lastKey = 'left'
             break
 
         case 83:
@@ -143,11 +315,12 @@ addEventListener('keydown', ({ keyCode }) => {
         case 68:
             console.log('right')
             keys.right.pressed = true
+            lastKey = 'right'
             break
 
         case 87:
             console.log('top')
-            player.velocity.y -= 20
+            player.velocity.y -= 40
             break
 
     }
@@ -173,7 +346,6 @@ addEventListener('keyup', ({ keyCode }) => {
 
         case 87:
             console.log('top')
-            player.velocity.y -= 20
             break
 
     }
